2b88230d837fab80ed3191e09b7d2ab5aefc7c99
Istam0808 <mamadaliyevistam@gmail.com>
2024-12-16 20:44:16 +0500

    JS

---
+++ JavaScript_Lesson/iTpark/lesson1.js
@@ -0,0 +1,27 @@
+let my_name = "Istam"
+let my_age = 29
+let my_xobbi = "Play Computer Games"
+let my_pet = "Dog"
+let my_pets_name = "Sharik"
+
+var my_friend = "Sardor"
+var my_friend_age = 29
+var my_friend_xobbi = "Play Computer Games"
+var my_friend_pet = "Dog"
+var my_friend_pets_name = "Sharik"
+
+const MY_MAKET = "Maket from FIGMA"
+const ABOUT = "Bellisimo Pizza"
+const LISTS = 10
+const COLOR = "Red"
+const MEDIA = "Media"
+
+
+
+
+
+
+
+console.log(`My Name is ${my_name} and I am ${my_age} years old. I have pet ${my_pet} , his name is ${my_pets_name}, 
+And i have frend ${my_friend} his a ${my_friend_age} years old, my friend hobbi is ${my_friend_xobbi} and he has pet ${my_friend_pet} and his pets name ${my_friend_pets_name}
+I have a ${MY_MAKET} About ${ABOUT} in this ${LISTS} Lists, in thsis most color is ${COLOR}, in his have a ${MEDIA}`)
