437b88ae2d10590ba77f25e5547e15bc800c654d
Istam0808 <mamadaliyevistam@gmail.com>
2024-12-18 21:58:54 +0500

    java_script

--- JavaScript_Lesson/iTpark/lesson1.js
+++ JavaScript_Lesson/iTpark/lesson1.js
@@ -1,27 +1,107 @@
-let my_name = "Istam"
-let my_age = 29
-let my_xobbi = "Play Computer Games"
-let my_pet = "Dog"
-let my_pets_name = "Sharik"
+// let my_name = "Istam"
+// let my_age = 29
+// let my_xobbi = "Play Computer Games"
+// let my_pet = "Dog"
+// let my_pets_name = "Sharik"
 
-var my_friend = "Sardor"
-var my_friend_age = 29
-var my_friend_xobbi = "Play Computer Games"
-var my_friend_pet = "Dog"
-var my_friend_pets_name = "Sharik"
+// var my_friend = "Sardor"
+// var my_friend_age = 29
+// var my_friend_xobbi = "Play Computer Games"
+// var my_friend_pet = "Dog"
+// var my_friend_pets_name = "Sharik"
 
-const MY_MAKET = "Maket from FIGMA"
-const ABOUT = "Bellisimo Pizza"
-const LISTS = 10
-const COLOR = "Red"
-const MEDIA = "Media"
-
-
+// const MY_MAKET = "Maket from FIGMA"
+// const ABOUT = "Bellisimo Pizza"
+// const LISTS = 10
+// const COLOR = "Red"
+// const MEDIA = "Media"
 
 
 
+// console.log(`My Name is ${my_name} and I am ${my_age} years old. I have pet ${my_pet} , his name is ${my_pets_name}, 
+// And i have frend ${my_friend} his a ${my_friend_age} years old, my friend hobbi is ${my_friend_xobbi} and he has pet ${my_friend_pet} and his pets name ${my_friend_pets_name}
+// I have a ${MY_MAKET} About ${ABOUT} in this ${LISTS} Lists, in thsis most color is ${COLOR}, in his have a ${MEDIA}`)
 
 
-console.log(`My Name is ${my_name} and I am ${my_age} years old. I have pet ${my_pet} , his name is ${my_pets_name}, 
-And i have frend ${my_friend} his a ${my_friend_age} years old, my friend hobbi is ${my_friend_xobbi} and he has pet ${my_friend_pet} and his pets name ${my_friend_pets_name}
-I have a ${MY_MAKET} About ${ABOUT} in this ${LISTS} Lists, in thsis most color is ${COLOR}, in his have a ${MEDIA}`)
+
+
+
+
+// let string  = "text  =>  текст"
+// DATA TYPES
+// 1. String    =>   текст
+// 2. Number    =>   числа
+// 3. Bigint    =>   большие цыфры
+// 4. Boolean   =>   true/false  (истина / ложь) (haqiqtan / yolg'on)
+// 5. Undefined =>   не определён  (not defined) (aniqlanmagan)
+// 6. Null      =>   пусто  (empty) (bo'sh)
+// 7. Symbol    =>   символ
+// 8. Object    =>   объект 
+// -------------------------------------------------------
+// "use strict"
+// This is strict mode that helps to avoid mistakes
+// -------------------------------------------------------
+// * String  ==  "...."  ==  simple text  == обычний текст
+// -------------------------------------------------------
+// * "",   '',   ``   =>  these quotes are used 
+//                      to create a string
+// -------------------------------------------------------
+// * Number  ==  1, 2, 3, ...
+// -------------------------------------------------------
+// * VARIABLES
+// 1. var       =>  5%
+// 2. let       =>  55%
+// 3. const     =>  40%
+
+// var   =>  global variable
+//           can be updated anytime anywhere with/without
+//           keyword var
+//           can leave block
+
+// let   =>  local variable
+//           can NOT be updated anytime anywhere with
+//           keyword let but only possible without 'let'
+//           can NOT leave block
+
+// const =>  local variable
+//           can NOT be updated
+//           can NOT leave block
+// ----------------------------------------------
+// * ---- SYNTAX ----
+// keyword variable-name = value
+// ключавое-слово имя-переменной = значение
+// ----------------------------------------------
+// * Variables inside of BLOCK OF CODE
+// {
+//     var test1 = "var test"
+//     let test2 = "let test"
+//     const TEST3 = "const TEST"
+// }
+// console.log(test1)
+// console.log(test2)
+// console.log(TEST3)
+// ----------------------------------------------
+// * CASES  =>  Стили надписи имён
+// camelCase  =>  ...^..^.. =>  greatBigAndBeautifulCity
+// snake_case =>  ..._..._. =>  great_big_and_beautiful_
+// PascalCase =>  ^..^..^.. =>  GreatBigAndBeautifulCity
+// kebab-case =>  ..-..-... =>  great-big-and-beautiful-
+
+// ----------------------------------------------
+// console.log(...) => shows a message inside of console
+//                     показывает сообщение в консоли
+//                     нужен для проверки кода
+// ----------------------------------------------
+// var test = "test"
+// `... ${test} ... `
+
+// let myName = 'Alisher'
+// let myAge = 28
+// console.log(`
+//     My name is ${myName}. 
+//     I am ${myAge} years old
+// `)
+// ----------------------------------------------
+// let myName = "Alisher"
+// let info = `This is my first class. My name is ${myName}`
+// console.log(info)
---
+++ JavaScript_Lesson/iTpark/lesson2.js
@@ -0,0 +1,4 @@
+let x = 5
+
+x = typeof(5) == parseInt( 2 * 2.5 )
+console.log(x)
