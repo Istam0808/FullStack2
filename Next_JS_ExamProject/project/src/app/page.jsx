01ea57d4da741140439eceb9f7cbf6cd390667e1
Istam <mamadaliyevistam@gmail.com>
2025-04-14 15:41:44 +0500

    home_page_and_layout_updates

--- Next_JS_ExamProject/project/src/app/page.jsx
+++ Next_JS_ExamProject/project/src/app/page.jsx
@@ -1,13 +1,10 @@
 import "./home_style.scss"
-import LeftScreen from "./components/LeftScreen";
-import Left from "./components/LeftScreen";
 import RightScreen from "./components/RightScreen";
 
 function Home() {
   return ( 
     <div className="home">
-      <LeftScreen />
-      <RightScreen></RightScreen>
+      <RightScreen/>
     </div>
    );
 }
